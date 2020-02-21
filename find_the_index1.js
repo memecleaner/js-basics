fbdc28008229bf6f2fcda8091d21bc2d5dfddb46
coco <apple@Apples-MacBook-Air.local>
2020-02-21 23:12:29 +0700

    find index

--- find_the_index1.js
+++ find_the_index1.js
@@ -2,9 +2,7 @@
 // keyword google "Javascript array findIndex"
 // atau gunakan loops dan buat kondisi untuk mendapatkan indexnya.
 
-/* NB: 
-jika tidak menemukan indexnya return -1
-*/
+
 
 search([1, 5, 3], 5) // 1
 search([9, 8, 3], 3) // 2
