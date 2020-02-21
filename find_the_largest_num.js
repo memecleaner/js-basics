1790eb62930bff77fb40dd41f7a7a60064df7b5e
coco <apple@Apples-MacBook-Air.local>
2020-02-21 23:13:09 +0700

    find the largest nummber array

--- find_the_largest_num.js
+++ find_the_largest_num.js
@@ -1,6 +1,18 @@
 // buat sebuah function yang me-return angka paling besar dari sebuah array of numbers;
 
+function findLargestNum(arr){
+    let palingBesar = 0
+    for (let i=0; i<arr.length; i++){
+        if(arr[i] > palingBesar){
+            palingBesar=arr[i];
+        }
+    }
+    console.log(palingBesar);
+} 
 
+
+
+// udah di kerjain 
 
 // there are no negative numbers.
 findLargestNum([4, 5, 1, 3]) // 5
