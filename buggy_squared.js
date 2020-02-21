8a6f8bec17b2678fdde1cc64bf3c5cd2a2d452f5
coco <apple@Apples-MacBook-Air.local>
2020-02-21 23:10:02 +0700

    fixing bugs

--- buggy_squared.js
+++ buggy_squared.js
@@ -1,10 +1,22 @@
 // 2. dibawah ini adalah function untuk mencari kuadrat dari sebuah bilangan.
 // kamu diminta untuk "fixing bugs" dari code dibawah ini.
-function squaed(b) {
-	return $a*$a;
+
+
+function squared(b) {
+	console.log(b*b);
 }
 
 
 squared(5) // 25
 squared(9) // 81
 squared(100) // 10000
+
+
+function search(arrayAngka, cari){
+    let index = -1;
+    for(i=0;i<arrayAngka.length; i++){
+    if (cari == arrayAngka[i])
+         index = i;
+    }
+    for return index
+}
