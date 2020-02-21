858f539a1ac63df7829d99a40d6342281e710e94
coco <apple@Apples-MacBook-Air.local>
2020-02-21 23:11:26 +0700

    concat array

--- concat_two_arr.js
+++ concat_two_arr.js
@@ -8,7 +8,7 @@ function arrConcat(arr1, arr2) {
 
 // TIPS:
 // keyword google "Javascript Concat"
-
+// udah di kerjain
 arrConcat([1, 3, 5], [2, 6, 8]) // [1, 3, 5, 2, 6, 8]
 arrConcat([7, 8], [10, 9, 1, 1, 2]) // [7, 8, 10, 9, 1, 1, 2]
 arrConcat([4, 5, 1], [3, 3, 3, 3, 3]) // [4, 5, 1, 3, 3, 3, 3, 3]
